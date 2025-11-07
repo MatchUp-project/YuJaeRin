f98db0455144d1ee620d799770623f59801a59a9
유재린 <1nnwuk@gmail.com>
2025-11-07 14:06:43 +0900

    답변 엔티티 만들기3

--- sbb/src/main/java/com/mysite/sbb/Answer.java
+++ sbb/src/main/java/com/mysite/sbb/Answer.java
@@ -1,19 +1,21 @@
 package com.mysite.sbb;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 
 
 import lombok.Getter;
 import lombok.Setter;
 
-import org.springframework.data.annotation.CreatedDate;
+
 
 @Getter
 @Setter
@@ -23,12 +25,13 @@ public class Answer {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
+    @Column(length = 200)
+    private String subject;
     @Column(columnDefinition = "TEXT")
     private String content;
 
-    @CreatedDate
     private LocalDateTime createDate;
 
-    @ManyToOne
-    private Question question;
+    @OneToMany(mappedBy = "question", cascade = CascadeType.REMOVE)
+    private List<Answer> answerList;
 }
